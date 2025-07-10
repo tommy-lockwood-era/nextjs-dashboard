8490a03888b1936f1b75fb0f0cae99a289fcda41
Tommy Lockwood <t.lockwood@erasolutions.us>
2025-07-09 19:37:37 -0500

    Add pages for dashboard, customers, and invoices

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return(
+        <>
+            <h1 className="font-bold text-2xl">Dashboard Page</h1>
+            <p>Welcome to the dashboard.</p>
+        </>
+    );
+}
