0457d9f3c90f4a3df0a54ba9ee57c5471ca3a09c
Tommy Lockwood <t.lockwood@erasolutions.us>
2025-07-17 20:40:56 -0500

    Gave login state more appropriate name

--- app/components/LoginButton/LoginButton.tsx
+++ app/components/LoginButton/LoginButton.tsx
@@ -2,11 +2,11 @@
 import { useState } from "react";
 
 export default function LoginButton() {
-    const [loggedIn, toggleLogin] = useState(false);
+    const [isLoggedIn, toggleLogin] = useState(false);
 
     return(
         <button onClick={() => toggleLogin((value) => !value)} className="ml-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-            {loggedIn ? "Logout" : "Login"}
+            {isLoggedIn ? "Logout" : "Login"} {/* JS ternary operator */}
         </button>
     );
 }
